74e992bd8682d9c211066c417aff0db601253606
Sten Otto Johnsen <sten.johnsen@bouvet.no>
2020-10-12 20:53:04 +0200

    Updated logging info

--- sync_accounts.py
+++ sync_accounts.py
@@ -11,7 +11,7 @@ from pprint import pprint
 from  Helpers import create_authenticated_http_session,get_accounts,get_transactions,getTransactionDate,getPayee,getMemo, getOut, getIn,getIntAmountMilli,getYnabTransactionDate,get_transactions_period,getYnabSyncId
 
 logging.basicConfig(filename='sync_accounts.log', filemode='w', level=logging.INFO)
-logging.info("Python version: %s, %s", platform.python_version(), platform.python_compiler())
+logging.info("Python version: %s, %s", platform.python_version(), platform.platform())
 
 def findMatchingTransfer(original_account, transaction, accounts_transactions_list, accounts, account_references):
     
